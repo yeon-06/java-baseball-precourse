c58495f490b08a924022cfb677f3066046f7a9f1
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-17 17:03:23 +0900

    refactor: 검증 메소드 Validator로 이동

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -8,7 +8,8 @@ public class Validator {
     private static final int CNT_NUMBER = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private static final String REGAX = "^["+MIN_NUMBER+"-"+MAX_NUMBER+"]*$";
+    private static final String INPUT_NUMBER_REGAX = "^[" + MIN_NUMBER + "-" + MAX_NUMBER + "]*$";
+    private static final String EXIT_CODE_REGAX = "[1-2]";
 
     public Validator() {
     }
@@ -17,18 +18,23 @@ public class Validator {
         return isRightLength(input) && isMinToMax(input) && isNotDuplicate(input);
     }
 
+    public boolean isRightAnswer(String input) {
+        Pattern pattern = Pattern.compile(EXIT_CODE_REGAX);
+        return pattern.matcher(input).matches();
+    }
+
     private boolean isRightLength(String input) {
         return input.length() == CNT_NUMBER;
     }
 
     private boolean isMinToMax(String input) {
-        Pattern numberPattern = Pattern.compile(REGAX);
+        Pattern numberPattern = Pattern.compile(INPUT_NUMBER_REGAX);
         return numberPattern.matcher(input).matches();
     }
 
     private boolean isNotDuplicate(String input) {
         Set<Character> set = new HashSet<>();
-        for(char s: input.toCharArray()) {
+        for (char s : input.toCharArray()) {
             set.add(s);
         }
         return input.length() == set.size();
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,8 +3,6 @@ package baseball.view;
 import baseball.utils.Validator;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.regex.Pattern;
-
 public class InputView {
     private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
     private static final String EXIT_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
@@ -17,7 +15,7 @@ public class InputView {
     public String inputAnswer() {
         System.out.print(INPUT_SENTENCE);
         String input = Console.readLine();
-        if(!validator.isValidString(input)) {
+        if (!validator.isValidString(input)) {
             throw new IllegalArgumentException();
         }
         return input;
@@ -26,14 +24,9 @@ public class InputView {
     public String inputExit() {
         System.out.println(EXIT_SENTENCE);
         String input = Console.readLine();
-        if(!isRightAnswer(input)) {
+        if (!validator.isRightAnswer(input)) {
             throw new IllegalArgumentException();
         }
         return input;
-    }
-
-    private boolean isRightAnswer(String input) {
-        Pattern pattern = Pattern.compile("[1-2]");
-        return pattern.matcher(input).matches();
     }
 }
